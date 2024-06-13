2b3abca58bb97f6cdedf3c30a8c827f4ff4028e0
Robert Fejer <robert.fejer@garmin.com>
2024-06-13 12:20:51 +0300

    Added initial model classes

---
+++ ggg_board_game/lib/models/team.dart
@@ -0,0 +1,10 @@
+import 'package:ggg_board_game/models/acrobat.dart';
+import 'package:ggg_board_game/models/archer.dart';
+import 'package:ggg_board_game/models/architect.dart';
+
+class Team {
+
+  Archer archer = Archer();
+  Architect architect = Architect();
+  Acrobat acrobat = Acrobat();
+}
